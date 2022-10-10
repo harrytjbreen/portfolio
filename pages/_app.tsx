c3bd49a34219332dc982672088cb5b99beda493b
harrybreen <harry.breen@ospreycharging.co.uk>
2022-10-10 23:33:43 +0100

    fix: removed ;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,7 +33,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
