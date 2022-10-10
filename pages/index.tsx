8271b7fb56897999f4be4d3698188dbc6bb88f00
harrybreen <harry.breen@ospreycharging.co.uk>
2022-10-10 23:12:25 +0100

    feat: added more meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,16 +42,27 @@ const Home: NextPage = () => {
           <title>Harry Breen</title>
           <meta name="description" content="Harry Breen's Portfolio" />
           <meta name="author" content="Harry Breen" />
-          <meta property="og:url" content="harrybreen.co.uk" />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content="Harry Breen's Portfolio" />
+          <meta property="og:url" content="harrybreen.co.uk" key="ogurl" />
+          <meta property="og:type" content="website" key="ogtype" />
+          <meta
+            property="og:site_name"
+            content="Harry Breen's Portfolio"
+            key="ogsitename"
+          />
+          <meta
+            property="og:title"
+            content="Harry Breen's Portfolio"
+            key="ogtitle"
+          />
           <meta
             property="og:description"
             content="The place to see all of Harry's web based work"
+            key="ogdesc"
           />
           <meta
             property="og:image"
-            content={require("../public/images/headshot.jpeg")}
+            content="https://harrybreen.co.uk/images/headshot.jpeg"
+            key="ogimg"
           />
           <link rel="icon" href="/favicon.ico" />
         </Head>
